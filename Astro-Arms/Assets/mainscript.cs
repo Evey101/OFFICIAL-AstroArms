0518fcb7864e597cceb7c44f670697ffb9cac534
Eve Ortiz <eveortiz.sia@gmail.com>
2019-01-14 16:59:45 -0500

    Updated Grabbing and Throwing
    Less wonky- the grabbing and throwing actually works now!!!!

--- Astro-Arms/Assets/mainscript.cs
+++ Astro-Arms/Assets/mainscript.cs
@@ -9,6 +9,7 @@ public class mainscript : MonoBehaviour
     public GameObject player;
    // public GameObject item;
     public int grabbing;
+    public bool following;
     public int powerID;
 
 	// Use this for initialization
@@ -17,12 +18,14 @@ public class mainscript : MonoBehaviour
         vert = new Vector2(0, .15f); // up and down speed
         horz = new Vector2(.15f, 0);// left and right speed
         grabbing = 0;
+        powerID = 0;
+        player.GetComponent<SpriteRenderer>().color = Color.white;
+        following = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        itemFunction();
         if (Input.GetKey(up))
         {
             player.transform.Translate(vert); // moving up
@@ -39,10 +42,42 @@ public class mainscript : MonoBehaviour
         {
             player.transform.Translate(horz); // moving right
         }
+        if (Input.GetKeyDown(grab))
+        {
+            grabbing += 1; 
+        }
+       
+        if (grabbing == 0)
+        {
+            player.GetComponent<SpriteRenderer>().color = Color.white; //the color white is its standard state
+        }
+
+        if (grabbing == 1) // this means that the key was pressed once, meaning it was used
+            //right now, its just showing through the color change.
+        {
+            if (powerID == 1)
+            {
+                player.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+            if (powerID == 2)
+            {
+                player.GetComponent<SpriteRenderer>().color = Color.blue;
+            }
+            if (powerID == 3)
+            {
+                player.GetComponent<SpriteRenderer>().color = Color.green;
+            }
+        }
+        if (grabbing == 2) // this means that the key was pressed a second time, throwing the powerup and setting the player back to 
+            //to its standard state
+        {
+            grabbing = 0;
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         if (other.gameObject.tag == "red" && grabbing == 0)
         {
             powerID = 1; 
@@ -57,46 +92,6 @@ public class mainscript : MonoBehaviour
         }
 
         //The collider is bigger than the actual player so that when the player gets close enough to a certain powerup, 
-        //it recognizes which powerup is closeby
-
-    }
-
-    void itemFunction()
-    {
-        if (Input.GetKeyDown(grab) && grabbing == 0)
-        {
-            grabbing = 1;
-        }
-        if (grabbing == 1)
-        {
-            if (powerID == 1)
-            {
-                player.GetComponent<SpriteRenderer>().color = Color.red;
-            }
-            if (powerID == 2)
-            {
-                player.GetComponent<SpriteRenderer>().color = Color.blue;
-            }
-            if (powerID == 3)
-            {
-                player.GetComponent<SpriteRenderer>().color = Color.green;
-            }
-            if (Input.GetKeyDown(grab))
-            {
-                powerID = 0;
-                player.GetComponent<SpriteRenderer>().color = Color.white;
-                grabbing = 0;
-            }
-        }
-        //when the player clicks the use button, the ship's color will change accordingly
-        //to which power up is close by
-
-        if (Input.GetKeyDown(grab) && grabbing == 2)
-        {
-            powerID = 0;
-            player.GetComponent<SpriteRenderer>().color = Color.white;
-            grabbing = 0;
-        }
-        // when the player presses the 
+        //it recognizes which powerup is close by
     }
 }
