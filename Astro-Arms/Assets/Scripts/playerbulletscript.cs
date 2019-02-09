61c9942aa81f3ea7fd3e9b037083cc8274acec70
Eve Ortiz <eveortiz.sia@gmail.com>
2019-02-09 13:47:27 -0500

    added bullet

---
+++ Astro-Arms/Assets/Scripts/playerbulletscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerbulletscript : MonoBehaviour 
+{
+    public Vector2 speed;
+
+	// Use this for initialization
+	void Start () 
+    {
+        speed = new Vector2(10, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        GetComponent<Rigidbody2D>().velocity = speed;
+	}
+}
