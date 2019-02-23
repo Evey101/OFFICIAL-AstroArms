59de95c3e7b6f1713e325ccbd06ed372649af87e
sadiul <sadiulakhanji.sia@gmail.com>
2019-02-23 15:13:33 -0500

    fixed assets and errors

--- Astro-Arms/Assets/Scripts/background.cs
+++ Astro-Arms/Assets/Scripts/background.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class background : MonoBehaviour 
 {
-    public GameObject  spaceScrolling,spaceScrolling1;
+    public GameObject  spaceScrolling;
     public Vector2 spaceSrollSpd;
 	// Use this for initialization
 	void Start () 
     {
-        spaceSrollSpd = new Vector2(0, -40 * Time.deltaTime);
+        spaceSrollSpd = new Vector2(0, -20 * Time.deltaTime);
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
         spaceScrolling.transform.Translate(spaceSrollSpd);
-        spaceScrolling1.transform.Translate(spaceSrollSpd);
 	}
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "space")
         {
-            collision.gameObject.transform.position = new Vector3(0, 15, 0);
+            collision.gameObject.transform.position = new Vector3(0, 5, 0);
         }
  
     }
